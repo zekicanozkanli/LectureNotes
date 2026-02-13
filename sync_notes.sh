2d0aee7525048d079923764af47ebb656ed6319a
zekicanozkanli <117727608+zekicanozkanli@users.noreply.github.com>
2026-02-13 20:04:29 +0300

    Auto-sync: 26/Feb/13 20:04:29

--- sync_notes.sh
+++ sync_notes.sh
@@ -1,10 +1,13 @@
-#!/zsh
-cd "/Users/markovchain_/Desktop/Bellek/Lecture Notes"
+#!/bin/zsh
+export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+cd "/Users/markovchain_/Library/Mobile Documents/com~apple~CloudDocs/Bellek/Lecture Notes"
 
 git add .
 git pull --rebase origin main
 
 if ! git diff-index --quiet HEAD; then
-    git commit -m "Auto-Sync: last update: $(date '+%y/%b/%d %H:%M:%S')"
+    git commit -m "Auto-sync: $(date '+%y/%b/%d %H:%M:%S')"
     git push origin main
 fi
+
