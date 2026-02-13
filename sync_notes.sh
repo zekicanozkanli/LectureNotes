b9e9b6c2ac5aa10c024189e893e7a4bae2dc5ba9
zekicanozkanli <117727608+zekicanozkanli@users.noreply.github.com>
2026-02-13 20:28:12 +0300

    Auto-Sync: last update:  Feb 13,  20:28

--- sync_notes.sh
+++ sync_notes.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+
+# Needed to be explicitly stated for background execution (from iCloud) 
 export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
 cd "/Users/markovchain_/Library/Mobile Documents/com~apple~CloudDocs/Bellek/Lecture Notes"
@@ -7,7 +9,7 @@ git add .
 git pull --rebase origin main
 
 if ! git diff-index --quiet HEAD; then
-    git commit -m "Auto-Sync: last update:  $(date '+%b %d  %H:%M')"
+    git commit -m "Auto-Sync: last update:  $(date '+%b %d,  %H:%M')"
     git push origin main
 fi
 
